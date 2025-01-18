c9c6c5ec023df22f17abd9d06fce6131fd3c373a
Carlos Ramírez <cramirezmun@unal.edu.co>
2025-01-18 18:10:23 -0500

    add background and slingshot images

--- Model.js
+++ Model.js
@@ -1,58 +1,55 @@
-class Box{
-  constructor(x,y,w,h,img,options={}){
-    this.body = Bodies.rectangle(
-      x,y,w,h, options);
+class Box {
+  constructor(x, y, w, h, img, options = {}) {
+    this.body = Bodies.rectangle(x, y, w, h, options);
     this.w = w;
     this.h = h;
     this.img = img;
     World.add(world, this.body);
-}
-  
-  show(){
+  }
+
+  show() {
     push();
-    translate(
-      this.body.position.x,
-      this.body.position.y);
+    translate(this.body.position.x, this.body.position.y);
     rotate(this.body.angle);
-    imageMode(CENTER)
-    image(this.img, 0,0,this.w,this.h);
-     pop();
+    imageMode(CENTER);
+    image(this.img, 0, 0, this.w, this.h);
+    pop();
   }
 }
 
 class Ground extends Box {
-   constructor(x,y,w,h,img){
-     super(x,y,w,h,img, {isStatic: true});
-   }
+  constructor(x, y, w, h, img) {
+    super(x, y, w, h, img, { isStatic: true });
+  }
 }
 
 class Bird {
-  constructor(x,y,r,mass,img){
-    this.body = Bodies.circle(
-      x, y, r, {
+  constructor(x, y, r, mass, img) {
+    this.body = Bodies.circle(x, y, r, {
       restitution: 0.7,
       collisionFilter: {
-        category: 2
-      }
+        category: 2,
+      },
     });
     this.img = img;
     Body.setMass(this.body, mass);
-    World.add(world, this.body);  
+    World.add(world, this.body);
   }
-  
-  show(){
+
+  show() {
     push();
     imageMode(CENTER);
-    translate(this.body.position.x,
-      this.body.position.y);
+    translate(this.body.position.x, this.body.position.y);
     rotate(this.body.angle);
-    image(this.img,
-      0,0,
-      2*this.body.circleRadius,
-      2*this.body.circleRadius);
-    pop();  
+    image(
+      this.img,
+      0,
+      0,
+      2 * this.body.circleRadius,
+      2 * this.body.circleRadius
+    );
+    pop();
   }
-    
 }
 
 class SlingShot {
@@ -60,34 +57,42 @@ class SlingShot {
     this.sling = Constraint.create({
       pointA: {
         x: bird.body.position.x,
-        y: bird.body.position.y
+        y: bird.body.position.y,
       },
       bodyB: bird.body,
       stiffness: 0.05,
-      length: 5
-     });
-     World.add(world, this.sling);
+      length: 5,
+    });
+    this.img = slingShot;
+    World.add(world, this.sling);
   }
-  
+
   show() {
-    if(this.sling.bodyB){
+    if (this.sling.bodyB) {
+      push();
+      imageMode(CENTER);
+      image(this.img, this.sling.pointA.x, this.sling.pointA.y, 75, 150);
+      pop();
       line(
-      this.sling.pointA.x,
-      this.sling.pointA.y,
-      this.sling.bodyB.position.x,
-      this.sling.bodyB.position.y)
+        this.sling.pointA.x,
+        this.sling.pointA.y,
+        this.sling.bodyB.position.x,
+        this.sling.bodyB.position.y
+      );
     }
   }
-  
-  fly(mc){
-    if(this.sling.bodyB && 
-       mc.mouse.button === -1 &&
-       this.sling.bodyB.position.x > this.sling.pointA.x + 10){
-      this.sling.bodyB.collisionFilter.category = 1;  
+
+  fly(mc) {
+    if (
+      this.sling.bodyB &&
+      mc.mouse.button === -1 &&
+      this.sling.bodyB.position.x > this.sling.pointA.x + 10
+    ) {
+      this.sling.bodyB.collisionFilter.category = 1;
       this.sling.bodyB = null;
     }
   }
-  
+
   attach(bird) {
     this.sling.bodyB = bird.body;
   }
